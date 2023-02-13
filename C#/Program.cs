8b6ac3a35a465ee22781ebb7beea30b083466022
Teo-Ingerman <Teo.ingerman@elev.ga.ntig.se>
2023-02-13 13:27:12 +0100

    changes

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,20 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+using System;
 
 class Program
 {
     static void Main()
     {
         
+        Winner writer = new Winner();
 
-
-
-
-        // Winner mister_ben = new Winner();
-        
-
-        Winner.celebration("ben");
+        writer.print("hello");
 
     }
 }
--- C#/Winner.cs
+++ C#/Winner.cs
@@ -1,2 +1,10 @@
 
+class Winner
+{
+    public void print(string text)
+    {
+        Console.Write(text);
+    } 
 
+
+}
